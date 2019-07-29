07ca58a57ff56ff467e06078c51b812a12392ebd
ivanov-an <an_ivanov@mail.ru>
2019-07-30 00:18:25 +0300

    work in progress

--- source/common/tracing_synapse/synapse_tracer_impl.cc
+++ source/common/tracing_synapse/synapse_tracer_impl.cc
@@ -1,5 +1,6 @@
 #include "common/tracing_synapse/synapse_tracer_impl.h"
 
+#include <regex>
 #include <string>
 
 #include "common/access_log/access_log_formatter.h"
@@ -31,6 +32,9 @@ static bool endsWith(std::string const &str, std::string const &suffix) {
 
 const std::string SynapseTracerUtility::SynapsePrefix = "x-synapse-";
 const std::string SynapseTracerUtility::BinarySuffix = "-bin";
+const std::regex SynapseTracerUtility::SynapseHeaderRegex("^x-synapse-(.+?)([-_]bin)?$");
+const std::regex SynapseTracerUtility::HeaderNameFilterRegex("-");
+
 
 void SynapseTracerUtility::fillSpan(Span& span, const Http::HeaderMap* request_headers,
                                     const Http::HeaderMap* response_headers,
@@ -54,15 +58,15 @@ void SynapseTracerUtility::addSynapseTagsFromHeaders(Span& span, const Http::Hea
   headers->iterate(
     [](const Http::HeaderEntry& header, void* context) -> {
       std::string key = std::string(header.key().getStringView());
-      if (startsWith(key, SynapsePrefix)) {
-        if (endsWith(key, BinarySuffix)) {
-          // TODO
-        } else {
-          // TODO
-          span.setTag(Tracing::Tags::get().GuidXRequestId, std::string(header.value().getStringView()));
-
+      std::match match;
+      if (std::regex_match(key, match, SynapseTracerUtility::SynapseHeaderRegex)) {
+        std::string tagName = "synapse." + std::regex_replace(match[1], SynapseTracerUtility::HeaderNameFilterRegex, "_");
+        if (match.size() > 2) {
+          tagName += "_bin";
         }
-      }
+        span.setTag(headerNameToTagName(tagName),
+                    std::string(header.value().getStringView()));
+      }   
       return Http::HeaderMap::Iterate::Continue;
     },
     &span
@@ -70,7 +74,13 @@ void SynapseTracerUtility::addSynapseTagsFromHeaders(Span& span, const Http::Hea
 }
 
 std::string& SynapseTracerUtility::headerNameToTagName(const std::string& headerName) {
-
+  std::match match;
+  if (std::regex_match(headerName, match, SynapseTracerUtility::SynapseHeaderRegex)) {
+    std::string tagName = "synapse." + std::regex_replace(match[1], SynapseTracerUtility::HeaderNameFilterRegex, "_");
+    if (match.size() > 2) {
+      tagName += "_bin";
+    }
+  }
 }
 
 } // namespace Tracing
